e611e2e34fb3acf3f988ee18390df604f6ca2a41
jackmerseal <jack_merseal@insideranken.org>
2024-03-27 10:13:20 -0500

    Changes

---
+++ QuarterlySales/Models/SaleViewModel.cs
@@ -0,0 +1,21 @@
+﻿namespace QuarterlySales.Models
+{
+	public class SaleViewModel : Sale
+	{
+		public IEnumerable<Employee> Employees { get; set; }
+		public IEnumerable<Sale> Sales { get; set; }
+		public Employee Employee { get; set; }
+		public Sale Sale { get; set; }
+		public int Quarter { get; set; }
+		public int Year { get; set; }
+		public decimal Amount { get; set; }
+		public int EmployeeId { get; set; }
+		public int SaleId { get; set; }
+
+		public SaleViewModel()
+		{
+			Employees = new List<Employee>();
+			Sales = new List<Sale>();
+		}
+	}
+}
---
+++ QuarterlySales/Models/UniqueManagerAttribute.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace QuarterlySales.Models
+{
+	public class UniqueManagerAttribute : ValidationAttribute
+	{
+		protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+		{
+			var employee = (Employee)validationContext.ObjectInstance;
+			var salesContext = (SalesContext)validationContext.GetService(typeof(SalesContext));
+			var existingManager = salesContext.Employees.FirstOrDefault(e => e.EmployeeId == employee.ManagerId &&
+																		 e.Firstname == employee.Firstname &&
+																		 e.Lastname == employee.Lastname &&
+																		 e.Birthdate == employee.Birthdate);
+
+			if (existingManager != null)
+			{
+				return new ValidationResult("Manager and employee can't be the same person.");
+			}
+			return ValidationResult.Success!;
+		}
+	}
+}
