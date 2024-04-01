51abbec149f3adb17d7cfd6f132db5d42b50cc64
Antoine Crenshaw <111997858+jackmerseal@users.noreply.github.com>
2024-04-01 11:49:12 -0500

    Got employee name to display in attribute

--- QuarterlySales/Models/UniqueSalesAttribute.cs
+++ QuarterlySales/Models/UniqueSalesAttribute.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.ComponentModel.DataAnnotations;
+using System.Linq;
+
 namespace QuarterlySales.Models
 {
 	public class UniqueSalesAttribute : ValidationAttribute
@@ -8,16 +10,19 @@ namespace QuarterlySales.Models
 		{
 			var sale = (Sale)validationContext.ObjectInstance;
 			var dbContext = (SalesContext)validationContext.GetService(typeof(SalesContext));
+
 			var existingSale = dbContext.Sales
-			.FirstOrDefault(s =>
+				.Where(s =>
 					s.EmployeeId == sale.EmployeeId &&
 					s.Year == sale.Year &&
 					s.Quarter == sale.Quarter &&
-					s.SaleId != sale.SaleId); 
+					s.SaleId != sale.SaleId)
+				.Join(dbContext.Employees, s => s.EmployeeId, e => e.EmployeeId, ( s, e ) => new { Sale = s, EmployeeName = e.Name })
+				.FirstOrDefault();
 
 			if (existingSale != null)
 			{
-				return new ValidationResult($"Sales for {sale.Employee?.Firstname} for {sale.Year} Q{sale.Quarter} are already in the database");
+				return new ValidationResult($"Sales for {existingSale.EmployeeName} for {sale.Year} Q{sale.Quarter} are already in the database");
 			}
 
 			return ValidationResult.Success;
